2841791803d1c12a84f3a9b4f4678587bdb280e3
ckelsel <ckelsel@hotmail.com>
2015-10-05 12:34:01 +0800

    Author: ckelsel Description: 1. add files

---
+++ training/app/src/main/java/com/ckelsel/android/training/MainActivity.java
@@ -0,0 +1,71 @@
+package com.ckelsel.android.training;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewConfiguration;
+
+import java.lang.reflect.Field;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getThreeDots();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void getThreeDots() {
+        try {
+            ViewConfiguration config = ViewConfiguration.get(this);
+            Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+
+            if (menuKeyField != null) {
+                menuKeyField.setAccessible(true);
+                menuKeyField.setBoolean(config, false);
+            }
+        }
+        catch (Exception e) {
+            // presumably, not relevant
+        }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Log.i("", "action_settings");
+                return true;
+                //break;
+            case R.id.action_search:
+                Log.i("", "action_search");
+                Intent intent = new Intent(this, ArticleActivity.class);
+                startActivity(intent);
+                return true;
+                //break;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
